75c8e8b7ba665e5a39dd1b72c0238d20387796c4
Dilshan <tdm.dev2@gmail.com>
2022-09-10 07:51:54 +0530

    Simplified the PDFViewActivity.java by removing extra code and pick file option code
    work perfectly

--- sample/src/main/java/com/github/barteksc/sample/PDFViewActivity.java
+++ sample/src/main/java/com/github/barteksc/sample/PDFViewActivity.java
@@ -1,11 +1,7 @@
 package com.github.barteksc.sample;
 
-import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.barteksc.pdfviewer.PDFView;
@@ -14,53 +10,31 @@ import com.github.barteksc.pdfviewer.util.FitPolicy;
 
 public class PDFViewActivity extends AppCompatActivity {
 
-    private static final String TAG = PDFViewActivity.class.getSimpleName();
-
-    private final static int REQUEST_CODE = 42;
-    public static final int PERMISSION_CODE = 42042;
-
-    public static final String SAMPLE_FILE = "sample.pdf";
-    public static final String READ_EXTERNAL_STORAGE = "android.permission.READ_EXTERNAL_STORAGE";
-
     PDFView pdfView;
-
-    Uri uri;
-
-    Integer pageNumber = 0;
-
-    String pdfFileName;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        System.out.println("onCreate ================================================================");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
-    public void setContentView(int layoutResID) {
-        System.out.println(layoutResID + "================================================================");
+    public void setContentView(int layoutResID) {   //   R.layout.activity_main = int layoutResID
+
+
+        //   01
         super.setContentView(layoutResID);
+
         this.pdfView = findViewById(R.id.pdfView);
-        afterViews();
-    }
 
-    void afterViews() {
-        pdfView.setBackgroundColor(Color.LTGRAY);
-        //if (uri != null) {
-        //displayFromUri(uri);
-        //} else {
-        displayFromAsset(SAMPLE_FILE);
-        //}
-        setTitle(pdfFileName);
-    }
 
-    private void displayFromAsset(String assetFileName) {
-        pdfFileName = assetFileName;
+        //   02
+        pdfView.setBackgroundColor(Color.CYAN);//.LTGRAY);
 
-        pdfView.fromAsset(SAMPLE_FILE)
-                .defaultPage(pageNumber)
+
+        //   03
+        pdfView.fromAsset("sample.pdf")
+                .defaultPage(10)
                 //.onPageChange(this)
                 .enableAnnotationRendering(true)
                 //.onLoad(this)
@@ -69,48 +43,5 @@ public class PDFViewActivity extends AppCompatActivity {
                 //.onPageError(this)
                 .pageFitPolicy(FitPolicy.BOTH)
                 .load();
-    }
-
-    private void displayFromUri(Uri uri) {
-        pdfFileName = getFileName(uri);
-
-        pdfView.fromUri(uri)
-                .defaultPage(pageNumber)
-                //.onPageChange(this)
-                .enableAnnotationRendering(true)
-                //.onLoad(this)
-                .scrollHandle(new DefaultScrollHandle(this))
-                .spacing(10) // in dp
-                //.onPageError(this)
-                .load();
-    }
-
-    //@OnActivityResult(REQUEST_CODE)
-    public void onResult(int resultCode, Intent intent) {
-        if (resultCode == RESULT_OK) {
-            uri = intent.getData();
-            displayFromUri(uri);
-        }
-    }
-
-
-    public String getFileName(Uri uri) {
-        String result = null;
-        if (uri.getScheme().equals("content")) {
-            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-            try {
-                if (cursor != null && cursor.moveToFirst()) {
-                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                }
-            } finally {
-                if (cursor != null) {
-                    cursor.close();
-                }
-            }
-        }
-        if (result == null) {
-            result = uri.getLastPathSegment();
-        }
-        return result;
     }
 }
